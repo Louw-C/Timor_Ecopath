52f344913e1d3a0f1935585a5b66b6241f2a48c4
Louw-C <74646083+Louw-C@users.noreply.github.com>
2024-05-17 15:04:00 +0900

    Added RColorBrewer package

---
+++ Code/Packages.R
@@ -0,0 +1,14 @@
+#Key packages to load
+
+library(readxl)
+library(dplyr)
+library(reshape)
+library(reshape2)
+library(ggplot2)
+library(car)
+library(tidyverse)
+library(wesanderson)
+library(RColorBrewer)
+
+library(plyr)
+library(Rmisc)
